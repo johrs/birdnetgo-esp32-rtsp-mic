be75f4334066f214037bed4b8408258112646a3e
Sukecz <suke.martin@gmail.com>
2026-05-09 11:05:08 +0200

    Delete esp32_rtsp_mic_birdnetgo directory

--- esp32_rtsp_mic_birdnetgo/WebUI.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-// Web UI (ESP32 RTSP Mic for BirdNET-Go): initialization and request handling
-void webui_begin();
-void webui_handleClient();
-
-// Push a log line from main into the Web UI ring buffer
-void webui_pushLog(const String &line);
